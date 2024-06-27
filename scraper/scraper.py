b04476b869c685669f053eac29cdbefc9ce111b9
Tennent <zalan.marton@gmail.com>
2024-06-27 11:01:52 +0200

    Define function to scrape target page content in json compatible form

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -7,3 +7,10 @@ from bs4 import BeautifulSoup
 def load_environment_variables():
     load_dotenv()
     return os.getenv('SCRAPE_URL')
+
+def scrape_content(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    content = soup.find_all('div', class_='item-outer-container')
+    content_list = [str(item) for item in content]
+    return content_list
