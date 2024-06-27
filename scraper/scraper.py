0f26c915d0aca435828dd1dc563981abab745486
Tennent <zalan.marton@gmail.com>
2024-06-27 17:16:19 +0200

    Rework scrape_content function to collect car model names and prices

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -9,11 +9,30 @@ def load_environment_variables():
     return os.getenv('SCRAPE_URL')
 
 def scrape_content(url):
-    res = requests.get(url)
+    res = requests.get(url) 
     soup = BeautifulSoup(res.content, 'html.parser')
-    content = soup.find_all('div', class_='item-outer-container')
-    content_list = [str(item) for item in content]
-    return content_list
+    content = soup.find_all('div', id='model-list')
+
+    result = []
+
+    if content:
+        for item in content:
+            car_info_list = item.find_all('div', class_='content')
+
+            if car_info_list:
+                for car_info in car_info_list:
+                    car_model_name = car_info.find('h2')
+                    car_model_price = car_info.find('strong')
+                    
+                    if car_model_name and car_model_price:
+                        car_data = {
+                            'model': car_model_name.get_text(strip=True),
+                            'price': car_model_price.get_text(strip=True)
+                        }
+                        if car_data not in result:
+                            result.append(car_data)
+
+    return result
 
 def handler(event, context):
     scrape_url = load_environment_variables()
