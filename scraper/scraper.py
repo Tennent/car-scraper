8e40902651695a6e6ceb7740ae56d0d422f2ef27
Tennent <zalan.marton@gmail.com>
2024-07-08 14:14:40 +0200

    Create separate function for collecting model details

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -18,26 +18,31 @@ def get_car_models(url):
     content = parsed_url.find_all('div', id='model-list')
     return content
 
+def get_car_model_details(url):
+    car_models = get_car_models(url)
+
+    if car_models:
+        for model in car_models:
+            car_details = model.find_all('div', class_='content')
+    
+    return car_details
+
 def scrape_content(url):
-    content = get_car_models(url)
+    car_details = get_car_model_details(url)
     result = []
 
-    if content:
-        for item in content:
-            car_info_list = item.find_all('div', class_='content')
-
-            if car_info_list:
-                for car_info in car_info_list:
-                    car_model_name = car_info.find('h2')
-                    car_model_price = car_info.find('strong')
+    if car_details:
+        for detail in car_details:
+            car_model_name = detail.find('h2')
+            car_model_price = detail.find('strong')
                     
-                    if car_model_name and car_model_price:
-                        car_data = {
-                            'model': car_model_name.get_text(strip=True),
-                            'price': car_model_price.get_text(strip=True)
-                        }
-                        if car_data not in result:
-                            result.append(car_data)
+            if car_model_name and car_model_price:
+                car_data = {
+                    'model': car_model_name.get_text(strip=True),
+                    'price': car_model_price.get_text(strip=True)
+                    }
+                if car_data not in result:
+                    result.append(car_data)
 
     return result
 
