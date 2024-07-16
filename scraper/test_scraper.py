51670a64eb9f2ae3ec5cb03141010b14b36ff92f
Tennent <zalan.marton@gmail.com>
2024-07-16 11:12:18 +0200

    Create unit test for get_car_model_name function

--- scraper/test_scraper.py
+++ scraper/test_scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from scraper import (
     get_car_models,
     get_car_model_details,
+    get_car_model_name,
 )
 
 def test_get_car_models():
@@ -19,3 +20,10 @@ def test_get_car_model_details():
     result = get_car_model_details(car_models)
     assert len(result) == 1
     assert result[0].get('class') == ['content']
+
+def test_get_car_model_name():
+    html = '<div class="content"><h2>Model Name</h2></div>'
+    soup = BeautifulSoup(html, 'html.parser')
+    car_details = soup.find_all('div', class_='content')
+    result = get_car_model_name(car_details)
+    assert result.text == 'Model Name'
