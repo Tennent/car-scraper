91b79cc27c8ad53ec8fb06fe9e0c6de7dc6fd244
Tennent <zalan.marton@gmail.com>
2024-07-18 12:53:50 +0200

    Create function to init and apply Lambda TF infrastructure

--- deploy.sh
+++ deploy.sh
@@ -57,6 +57,18 @@ image_uploader() {
     fi
 }
 
+# Init and apply Terraform for Lambda infrastructure
+create_lambda_infrastructure() {
+    local aws_region="$1"
+    local bucket_name="$2"
+    
+    ( cd ./infras/lambda_infra && terraform init -backend-config="$bucket_name" -backend-config="region=$aws_region" && terraform apply -auto-approve )
+    if [ $? -ne 0 ]; then
+        echo "Terraform apply for lambda_infra failed. Exiting."
+        exit 1
+    fi
+}
+
 deploy() {
     aws_region="$1"
     aws_account_id="$2"
