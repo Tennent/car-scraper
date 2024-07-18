3ccadce0fc90ba4ad11f38a4f3b0868215ac4d5d
Tennent <zalan.marton@gmail.com>
2024-07-18 10:46:27 +0200

    Define TF managed infrastructure for AWS ECR repository

---
+++ infras/repo_infra/repo.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "car-scraper-repo-bucket"
+    key    = "repo_infra/terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_ecr_repository" "car_scraper_lambda" {
+  name                 = "car_scraper_lambda"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.car_scraper_lambda.repository_url
+}
