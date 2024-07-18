e9bf970851242be13ad9e9692f2fa89cc05981f8
Tennent <zalan.marton@gmail.com>
2024-07-18 11:36:02 +0200

    Update repo infrastructure function variables

--- deploy.sh
+++ deploy.sh
@@ -19,8 +19,9 @@ create_s3_bucket() {
 # Init and apply Terraform for repo infrastructure
 create_repo_infrastructure() {
     local aws_region="$1"
+    local bucket_name="$2"
     
-    ( cd ./infras/repo_infra && terraform init -backend-config="bucket=car-scraper-repo-bucket" -backend-config="region=$aws_region" && terraform apply -auto-approve )
+    ( cd ./infras/repo_infra && terraform init -backend-config="bucket=$bucket_name" -backend-config="region=$aws_region" && terraform apply -auto-approve )
     if [ $? -ne 0 ]; then
         echo "Terraform apply for repo_infra failed. Exiting."
         exit 1
