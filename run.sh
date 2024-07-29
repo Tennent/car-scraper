1dc09b582dfdd980089431a7a599351bc2d69b14
Tennent <zalan.marton@gmail.com>
2024-07-29 09:11:13 +0200

    Modify script to deploy infrastructure if not yet present

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 run_lambda() {
-    aws lambda invoke --function-name car_scraper response.json 
+    aws_region="$1"
+    aws_account_id="$2"
+
+    aws lambda get-function --function-name car_scraper > /dev/null
+
+    if [ $? -ne 0 ]; then
+        echo "Lambda function not found. Creating..."
+        ./deploy.sh "$aws_region" "$aws_account_id"
+    else
+        echo "Lambda function found. Invoking..."
+        aws lambda invoke --function-name car_scraper response.json
+    fi
 }
 
-run_lambda
+run_lambda "$1" "$2"
