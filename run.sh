ac6b9e998d881785e9d7687897497b350b2fbfd5
Tennent <zalan.marton@gmail.com>
2024-08-21 13:27:52 +0200

    Remove conditional deployment and refactor

--- run.sh
+++ run.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 run_lambda() {
-    aws_region="$1"
-    aws_account_id="$2"
+  aws lambda get-function --function-name car_scraper > /dev/null
 
-    aws lambda get-function --function-name car_scraper > /dev/null
-
-    if [ $? -ne 0 ]; then
-        echo "Lambda function not found. Creating..."
-        ./deploy.sh "$aws_region" "$aws_account_id"
-    else
-        echo "Lambda function found. Invoking..."
-        aws lambda invoke --function-name car_scraper response.json
-    fi
+  if [ $? -ne 0 ]; then
+    echo "Lambda function not found. Exiting..."
+      exit 1
+  else
+    echo "Lambda function found. Invoking..."
+    aws lambda invoke --function-name car_scraper response.json
+  fi
 }
 
-run_lambda "$1" "$2"
+run_lambda
