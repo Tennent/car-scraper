6bc764f1df2a0fe0e73786bc257980f5d2d3da12
Tennent <zalan.marton@gmail.com>
2024-08-21 13:45:14 +0200

    Create function for AWS resource verification

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,70 @@
 #!/bin/bash
 
+check_aws_resources() {
+  #Flag to track if any resources exist
+  local resource_found=false
+
+  # Check if the S3 bucket exists
+  aws s3api head-bucket --bucket car-scraper-bucket 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "S3 bucket not found. Continuing..."
+  else
+    echo "S3 bucket found. Destroying..."
+    resource_found=true
+  fi
+
+  # Check if the ECR repository exists
+  aws ecr describe-repositories --repository-names car_scraper_lambda 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "ECR repository not found. Continuing..."
+  else
+    echo "ECR repository found. Destroying..."
+    resource_found=true
+  fi
+
+  # Check if the Lambda function exists
+  aws lambda get-function --function-name car_scraper 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "Lambda function not found. Continuing..."
+  else
+    echo "Lambda function found. Destroying..."
+    resource_found=true
+  fi
+
+  # Check if the IAM role exists
+  aws iam get-role --role-name lambda_role 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "IAM role not found. Continuing..."
+  else
+    echo "IAM role found. Destroying..."
+    resource_found=true
+  fi
+
+  # Check if the IAM policy exists
+  aws iam get-policy --policy-name dynamodb_lambda_policy 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "IAM policy not found. Continuing..."
+  else
+    echo "IAM policy found. Destroying..."
+    resource_found=true
+  fi
+
+  # Check if the DynamoDB table exists
+  aws dynamodb describe-table --table-name car_table 2>/dev/null
+  if [ $? -ne 0 ]; then
+    echo "DynamoDB table not found. Continuing..."
+  else
+    echo "DynamoDB table found. Destroying..."
+    resource_found=true
+  fi
+
+  if [ $resource_found = true ]; then
+    return 0  # At least one resource found
+  else
+    return 1  # No resources found
+  fi
+}
+
 delete_s3_bucket() {
     local region="$1"
     local bucket_name="car-scraper-bucket"
@@ -29,8 +94,14 @@ delete_s3_bucket() {
 cleanup() {
     aws_region="$1"
 
-    ( cd ./infras/lambda_infra && terraform destroy -auto-approve )
-    ( cd ./infras/repo_infra && terraform destroy -auto-approve )
+  check_aws_resources
+    if [ $? -eq 1 ]; then
+      echo "No AWS resources found. Exiting..."
+      exit 0
+    fi
+
+  ( cd ./infras/lambda_infra && terraform init -reconfigure && terraform destroy -auto-approve )
+  ( cd ./infras/repo_infra && terraform init -reconfigure && terraform destroy -auto-approve )
 
     delete_s3_bucket "$aws_region"
 }
