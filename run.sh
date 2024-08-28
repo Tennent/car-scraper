97057dbde4d64f0cb68ab81f9a23a2997754d259
Tennent <zalan.marton@gmail.com>
2024-08-28 12:53:50 +0200

    Correct Lambda function name

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 run_lambda() {
-  aws lambda get-function --function-name car_scraper > /dev/null
+  aws lambda get-function --function-name car_scraper_lambda > /dev/null
 
   if [ $? -ne 0 ]; then
     echo "Lambda function not found. Exiting..."
       exit 1
   else
     echo "Lambda function found. Invoking..."
-    aws lambda invoke --function-name car_scraper response.json
+    aws lambda invoke --function-name car_scraper_lambda response.json
   fi
 }
 
